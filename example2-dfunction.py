a411b821be24b5cdeaed99c7df11357688648222
BhappaYash <78207612+BhappaYash@users.noreply.github.com>
2022-07-10 00:10:05 +0530

    added 2nd line by techteam1

--- example2-dfunction.py
+++ example2-dfunction.py
@@ -4,3 +4,5 @@ print("Edited this file for the first time from the Github website.")
 print("Added this line of code by techdev from from git terminal")
 #-------------------------
 print("Added this 1st line by techteam1 from GitHub")
+#-------------------------
+print("Added this 2nd line by techteam1 from GitHub")
